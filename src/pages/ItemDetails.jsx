884232ea0476f36cad25c6564444f98c9de503fd
Airam Plasencia <airaplas@gmail.com>
2025-02-12 17:00:06 +0000

    New Task.json

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -12,22 +12,42 @@ const ItemDetails = ({ toDo }) => {
         return (
             <div>
                 <p>Task not found</p>
-                <button onClick={() => navigate('/')}>Go back to tasks list</button>
+                <button className="back-to-task-btn" onClick={() => navigate('/')}>Go back to tasks list</button>
             </div>
         );
     }
 
     return (
         <div>
-            <h2>Task Details</h2>
+             {task.title === "Read the project" && (
+                <div>
+                    <img 
+                        src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExejUzZHVkN28xOTdkajk0NjVsOTk0OXdzNWQ3bWljZjE1MTFsdmJsdiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/NFA61GS9qKZ68/giphy.gif"  
+                        alt="Task GIF"
+                        style={{ maxWidth: '300px', height: 'auto'}} 
+                    />
+                </div>
+            )}
+            
+            {task.title === "Create React to Do application" && (
+                <div>
+                    <img 
+                        src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExYXN6dXlidnRlcWUwYjVhcWs4YXdpYnIwMnRybm5vZTU0eTl5MzljNyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/citBl9yPwnUOs/giphy.gif"  
+                        alt="Create React Task GIF"
+                        style={{ maxWidth: '300px', height: 'auto'}} 
+                    />
+                </div>
+            )}
+            <h2 className='details'>Task Details</h2>
+            
             <p><strong>Title:</strong> {task.title}</p>
             <p><strong>Status:</strong> {task.status ? 'Completed' : 'Pending'}</p>
             <p><strong>Description:</strong> {task.description || 'No description available'}</p>
+           
             
-            <button onClick={() => navigate('/')}>Back to Task List</button>
+            <button className="back-to-task-btn" onClick={() => navigate('/')}>Back to Task List</button>
         </div>
     );
 }
 
-export default ItemDetails;
-
+export default ItemDetails
